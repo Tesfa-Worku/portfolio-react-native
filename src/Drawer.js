4c5a4d9ab93e134b5f4ec7e4226e7b5f22e8d0f5
Tesfa-Worku <hopegorg@gmail.com>
2022-01-29 00:00:05 -0500

    chore(drawer): add navigation

--- src/Drawer.js
+++ src/Drawer.js
@@ -1,9 +1,19 @@
 import { StyleSheet, View } from 'react-native';
+import Home from './Home';
+import { NavigationContainer } from '@react-navigation/native';
+import Stack from '@react-navigation/native-stack';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
 
 export default function Drawer() {
+    const Stack = createNativeStackNavigator();
+
     return (
-        <View style={styles.container}>      
-        </View>
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="Home" component={Home} />
+            </Stack.Navigator>
+        </NavigationContainer>
     );
 }
 
