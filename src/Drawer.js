75345334ace022f4e9a3fc336463a4c1a6c50fee
Tesfa-Worku <hopegorg@gmail.com>
2022-01-28 23:33:51 -0500

    chore(drawer): add start up content

---
+++ src/Drawer.js
@@ -0,0 +1,17 @@
+import { StyleSheet, View } from 'react-native';
+
+export default function Drawer() {
+    return (
+        <View style={styles.container}>      
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
