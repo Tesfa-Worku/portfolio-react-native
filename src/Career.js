9f056b889e28671db13b78db2e1aa32e0226594c
Tesfa-Worku <hopegorg@gmail.com>
2022-01-31 23:11:04 -0500

    chore(career): add new styles

--- src/Career.js
+++ src/Career.js
@@ -15,9 +15,9 @@ export default function Career() {
                 Bitwise Industrie's Apprenticeship program is built to bridge the gap between learning and working in the tech industry. Participants in this paid program work on real projects under the supervision of a seasoned technical lead. Apart from the day-to-day workload, apprentices will have several opportunities to sharpen their skills and give back to the larger tech community by leading workshops, meetings, and co-teaching classes.                            
             </Text>
 
-            <Text>
-                <Text>React</Text>
-                <Text>JavaScript</Text>
+            <Text style={styles.wrp2}>
+                <Text style={styles.wrp2}>React</Text>
+                <Text style={styles.wrp2}>JavaScript</Text>
                 <Text>css</Text>
                 <Text>HTML</Text>
                 <Text>Agile</Text>
@@ -36,8 +36,12 @@ export default function Career() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
+        flexDirection: 'column', 
+        backgroundColor: '#fff',            
         justifyContent: 'center',
+        marginTop: 100,            
     },
+    wrp2: {
+        flex: 0,
+    }
 });
