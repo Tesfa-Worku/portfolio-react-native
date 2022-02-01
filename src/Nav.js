8ec33c83432f4a495bac029a5d889ba782c3f24a
Tesfa-Worku <hopegorg@gmail.com>
2022-01-31 23:14:52 -0500

    chore(nav): refactor page

--- src/Nav.js
+++ src/Nav.js
@@ -1,92 +1,74 @@
-import { StyleSheet, View } from 'react-native';
 
-export default function Nav() {
-    return (
-        <View style={styles.container}>      
+import React from "react";
+import {
+  View,
+  StyleSheet,
+  Text,
+  Pressable,  
+} from "react-native";
+
+export default function Nav({ setPage }) {  
+  return (
+    <View style={styles.container}>
+      <View style={{color: 'white'}}>
+        <Pressable onPress={() => setPage("Home")}>
+          <View>
+            <View style={{color: 'white'}}>
+              <Text style={{color: 'white'}}>Tesfa Worku</Text>
+              <Text style={{color: 'white'}}>I Build things for the Web</Text>
+            </View>
+          </View>
+        </Pressable>
+      </View>
+      <View >
+        <View style={styles.cont2}>
+          <Pressable onPress={() => setPage("Home")}>
+            <Text style={{color: 'white'}} name="Home">
+              Home
+            </Text>
+          </Pressable>
+          <Pressable onPress={() => setPage("Portfolio")}>
+            <Text style={{color: 'white'}} name="Portfolio">
+              Portfolio
+            </Text>
+          </Pressable>
+          <Pressable onPress={() => setPage("Career")}>
+            <Text style={{color: 'white'}} name="Career">
+              Career
+            </Text>
+          </Pressable>
+          <Pressable onPress={() => setPage("Education")}>
+            <Text style={{color: 'white'}} name="Education">
+              Education
+            </Text>
+          </Pressable>          
         </View>
-    );
-}
+      </View>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
+  container: {
+    flex: 1,
+    flexGrow: 0,
+    padding: 80,
+    width: '100%',
+    backgroundColor: '#000',    
+    flexDirection: 'column',
+    // alignItems: 'flex-start',
+    // justifyContent: 'stretch',
+    marginTop: 10,        
+  },
+  cont2: {
+    flex: 1,
+    width: '100%',
+    flexGrow: 0,
+    flexDirection: 'row',
+    alignItems: 'flex-start',
+    justifyContent:'space-between',
+    marginTop: 20,
+    marginLeft: 20,
+    
+  }
 });
-
-
-// import { Link } from 'react-router-dom';
-
-// const Nav = ({ currentPage, setCurrentPage}) => {
-
-//     const classList = (pageName) => {
-//         return currentPage === pageName
-//             ? 'nav-link active'
-//             : 'nav-link';
-//     }
-
-//     return(
-//         <header className="d-flex flex-column justify-content-evenly bg-dark p-3">
-//             <nav className="navbar navbar-dark bg-dark flex-wrap h-100 w-100 align-content-end">
-//                 <Link
-//                     className="navbar-brand text-light p-lg-2"
-//                     to={"/"}
-//                     onClick={() => setCurrentPage('Profile')}
-//                 >
-//                     <span className="fs-3">Hi, I'm <strong>Tesfa Worku</strong></span><br/>
-//                     <small>I build things for the <strong>web</strong></small>
-//                 </Link>
-//                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mainMenu" aria-controls="mainMenu" aria-expanded="false" aria-label="Toggle navigation">
-//                     <span className="navbar-toggler-icon m-3 p-3"></span>
-//                 </button>
-//                 <div className="collapse navbar-collapse mt-lg-5" id="mainMenu">
-//                     <ul className="navbar-nav flex-column border-light border-top w-100">
-//                         <li className="nav-item border-light border-bottom">
-//                             <Link
-//                                 className={classList('Profile')}
-//                                 to={"/"}
-//                                 onClick={() => setCurrentPage('Profile')}>
-//                                     Profile
-//                             </Link>
-//                         </li>
-//                         <li className="nav-item border-light border-bottom">
-//                             <Link 
-//                                 className={classList('Career')} 
-//                                 to={"/career-development"} onClick={() => setCurrentPage('Career')}>
-//                                     Career Development
-//                             </Link>
-//                         </li>
-//                         <li className="nav-item border-light border-bottom">
-//                             <Link 
-//                                 className={classList('Portfolio')} 
-//                                 to={"/portfolio"} 
-//                                 onClick={() => setCurrentPage('Portfolio')}>
-//                                     Portfolio
-//                             </Link>
-//                         </li>
-//                         <li className="nav-item border-light border-bottom">
-//                             <Link 
-//                                 className={classList('Education')} 
-//                                 to={"/education"} 
-//                                 onClick={() => setCurrentPage('Education')}>
-//                                     Education
-//                             </Link>
-//                         </li>
-//                         <li className="nav-item border-light border-bottom">
-//                             <Link   
-//                                 className={classList('Contact')} 
-//                                 to={"/contact"} 
-//                                 onClick={() => setCurrentPage('Contact')}>
-//                                     Contact Tesfa
-//                             </Link>
-//                         </li>
-//                     </ul>
-//                 </div>                
-//             </nav>
-//         </header>
-//     )
-// }
-
-// export default Nav;
