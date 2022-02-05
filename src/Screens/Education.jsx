756738c2c7d5ef4bc9faf79a35a1cd688e0a8a63
Tesfa-Worku <hopegorg@gmail.com>
2022-02-04 23:26:48 -0500

    chore(education): add drawer content

---
+++ src/Screens/Education.jsx
@@ -0,0 +1,30 @@
+import { StyleSheet, View, Text } from 'react-native';
+
+export default function Education() {
+    return (
+        <View style={styles.container}>
+            <Text>Education</Text>
+            <View>
+                <Text><Text style={styles.bold}>Workforce training at Bitwise Industries - Certificate</Text>July, 2021 - August, 2021 Study JavaScript and React.
+                </Text>
+                <Text><Text style={styles.bold}>Evangadi Academy - Certificate</Text>February, 2021 - June, 2021 Full-Stack web development
+                </Text>
+                <Text><Text style={styles.bold}>Arba-Minch University - Bsc</Text> October, 2008 - July, 2011 B.Sc in Mechanical Engineering
+                </Text>                       
+            </View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: 'column', 
+        backgroundColor: '#fff',            
+        justifyContent: 'center',
+        marginTop: 100,            
+    },
+    bold: {
+        fontWeight: 'bold',
+    }
+});
