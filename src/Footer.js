bd77279ddc0d034e43f8ed7616e9ccab92b32dde
Tesfa-Worku <hopegorg@gmail.com>
2022-04-27 22:52:36 -0400

    refactor(footer): add footer component

--- src/Footer.js
+++ src/Footer.js
@@ -1,9 +1,11 @@
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
 
-export default function App() {
+export default function Footer() {
     return (
         <View style={styles.container}>
-
+            <Text style={styles.textColor}>1836 Metzerott Rd, Adelphi, MD 20783</Text>
+            <Text style={styles.textColor}>(240) 705-4466</Text>
+            <Text style={styles.textColor}>hopegorg@gmail.com</Text>
         </View>
     );
 }
@@ -11,29 +13,14 @@ export default function App() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#fff',
+        width: "100%",
+        maxHeight: "10%",
         alignItems: 'center',
-        justifyContent: 'center',
+        justifyContent: 'space-evenly',
+        backgroundColor: '#000',    
+        flexDirection: 'row',
     },
+    textColor: {
+        color: "#F2D379"
+    }
 });
-
-
-// import { Link } from 'react-router-dom';
-
-// const Footer = ({ setCurrentPage }) => {
-//     return(
-//         <footer className="fixed-bottom d-flex flex-column flex-md-row align-items-center justify-content-evenly w-100 bg-dark text-center">
-//             <div className="text-light">
-//                 1836 Metzerott Rd, Adelphi, MD 20783
-//             </div>
-//             <div className="text-light">
-//                 (240) 705-4466
-//             </div>
-//             <div className="text-light">
-//                 <Link to={"contact"}  className="btn btn-dark" onClick={() => setCurrentPage('Contact')}>hopegorg@gmail.com</Link>
-//             </div>
-//         </footer>
-//     )
-// }
-
-// export default Footer;
